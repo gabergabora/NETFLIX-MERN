270f94e9c8df27d21fcab9f0487fcb6046c9db75
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-04-16 02:28:25 +0500

    Navar & Featured components added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,121 +1,14 @@
 import React from "react";
 import "./Home.scss";
+
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
-        width="100%"
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet autem ab
-      odio dicta delectus. Rerum ipsum iusto ad culpa error, veritatis tenetur
-      quod debitis quia. Libero repellat labore assumenda voluptatibus. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Eos earum laborum
-      commodi! Tempora, ullam corrupti nostrum debitis eum iste blanditiis
-      minima ipsam laborum natus vitae est eaque ipsum? Facere, aperiam! Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Error amet sint esse,
-      nisi dolorem eius maiores quis tempore quia. Explicabo sint dolor ullam
-      eum ipsa nostrum ut ex neque cumque. Lorem, ipsum dolor sit amet
-      consectetur adipisicing elit. Nihil eaque vitae enim quibusdam illo, aut
-      unde iste, fugiat quasi sed doloremque tempore atque laboriosam voluptatum
-      inventore hic maiores perspiciatis voluptate. Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Eveniet autem ab odio dicta delectus. Rerum
-      ipsum iusto ad culpa error, veritatis tenetur quod debitis quia. Libero
-      repellat labore assumenda voluptatibus. Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Eos earum laborum commodi! Tempora, ullam
-      corrupti nostrum debitis eum iste blanditiis minima ipsam laborum natus
-      vitae est eaque ipsum? Facere, aperiam! Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Error amet sint esse, nisi dolorem eius
-      maiores quis tempore quia. Explicabo sint dolor ullam eum ipsa nostrum ut
-      ex neque cumque. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      Nihil eaque vitae enim quibusdam illo, aut unde iste, fugiat quasi sed
-      doloremque tempore atque laboriosam voluptatum inventore hic maiores
-      perspiciatis voluptate.Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Eveniet autem ab odio dicta delectus. Rerum ipsum iusto ad culpa
-      error, veritatis tenetur quod debitis quia. Libero repellat labore
-      assumenda voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Eos earum laborum commodi! Tempora, ullam corrupti nostrum debitis
-      eum iste blanditiis minima ipsam laborum natus vitae est eaque ipsum?
-      Facere, aperiam! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Error amet sint esse, nisi dolorem eius maiores quis tempore quia.
-      Explicabo sint dolor ullam eum ipsa nostrum ut ex neque cumque. Lorem,
-      ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque vitae enim
-      quibusdam illo, aut unde iste, fugiat quasi sed doloremque tempore atque
-      laboriosam voluptatum inventore hic maiores perspiciatis voluptate.Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Eveniet autem ab odio
-      dicta delectus. Rerum ipsum iusto ad culpa error, veritatis tenetur quod
-      debitis quia. Libero repellat labore assumenda voluptatibus. Lorem ipsum
-      dolor sit amet consectetur adipisicing elit. Eos earum laborum commodi!
-      Tempora, ullam corrupti nostrum debitis eum iste blanditiis minima ipsam
-      laborum natus vitae est eaque ipsum? Facere, aperiam! Lorem ipsum dolor
-      sit amet consectetur adipisicing elit. Error amet sint esse, nisi dolorem
-      eius maiores quis tempore quia. Explicabo sint dolor ullam eum ipsa
-      nostrum ut ex neque cumque. Lorem, ipsum dolor sit amet consectetur
-      adipisicing elit. Nihil eaque vitae enim quibusdam illo, aut unde iste,
-      fugiat quasi sed doloremque tempore atque laboriosam voluptatum inventore
-      hic maiores perspiciatis voluptate.Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Eveniet autem ab odio dicta delectus. Rerum ipsum iusto
-      ad culpa error, veritatis tenetur quod debitis quia. Libero repellat
-      labore assumenda voluptatibus. Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Eos earum laborum commodi! Tempora, ullam corrupti
-      nostrum debitis eum iste blanditiis minima ipsam laborum natus vitae est
-      eaque ipsum? Facere, aperiam! Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Error amet sint esse, nisi dolorem eius maiores quis
-      tempore quia. Explicabo sint dolor ullam eum ipsa nostrum ut ex neque
-      cumque. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
-      eaque vitae enim quibusdam illo, aut unde iste, fugiat quasi sed
-      doloremque tempore atque laboriosam voluptatum inventore hic maiores
-      perspiciatis voluptate.Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Eveniet autem ab odio dicta delectus. Rerum ipsum iusto ad culpa
-      error, veritatis tenetur quod debitis quia. Libero repellat labore
-      assumenda voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Eos earum laborum commodi! Tempora, ullam corrupti nostrum debitis
-      eum iste blanditiis minima ipsam laborum natus vitae est eaque ipsum?
-      Facere, aperiam! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Error amet sint esse, nisi dolorem eius maiores quis tempore quia.
-      Explicabo sint dolor ullam eum ipsa nostrum ut ex neque cumque. Lorem,
-      ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque vitae enim
-      quibusdam illo, aut unde iste, fugiat quasi sed doloremque tempore atque
-      laboriosam voluptatum inventore hic maiores perspiciatis voluptate.Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Eveniet autem ab odio
-      dicta delectus. Rerum ipsum iusto ad culpa error, veritatis tenetur quod
-      debitis quia. Libero repellat labore assumenda voluptatibus. Lorem ipsum
-      dolor sit amet consectetur adipisicing elit. Eos earum laborum commodi!
-      Tempora, ullam corrupti nostrum debitis eum iste blanditiis minima ipsam
-      laborum natus vitae est eaque ipsum? Facere, aperiam! Lorem ipsum dolor
-      sit amet consectetur adipisicing elit. Error amet sint esse, nisi dolorem
-      eius maiores quis tempore quia. Explicabo sint dolor ullam eum ipsa
-      nostrum ut ex neque cumque. Lorem, ipsum dolor sit amet consectetur
-      adipisicing elit. Nihil eaque vitae enim quibusdam illo, aut unde iste,
-      fugiat quasi sed doloremque tempore atque laboriosam voluptatum inventore
-      hic maiores perspiciatis voluptate.Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Eveniet autem ab odio dicta delectus. Rerum ipsum iusto
-      ad culpa error, veritatis tenetur quod debitis quia. Libero repellat
-      labore assumenda voluptatibus. Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Eos earum laborum commodi! Tempora, ullam corrupti
-      nostrum debitis eum iste blanditiis minima ipsam laborum natus vitae est
-      eaque ipsum? Facere, aperiam! Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Error amet sint esse, nisi dolorem eius maiores quis
-      tempore quia. Explicabo sint dolor ullam eum ipsa nostrum ut ex neque
-      cumque. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
-      eaque vitae enim quibusdam illo, aut unde iste, fugiat quasi sed
-      doloremque tempore atque laboriosam voluptatum inventore hic maiores
-      perspiciatis voluptate.Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Eveniet autem ab odio dicta delectus. Rerum ipsum iusto ad culpa
-      error, veritatis tenetur quod debitis quia. Libero repellat labore
-      assumenda voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Eos earum laborum commodi! Tempora, ullam corrupti nostrum debitis
-      eum iste blanditiis minima ipsam laborum natus vitae est eaque ipsum?
-      Facere, aperiam! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Error amet sint esse, nisi dolorem eius maiores quis tempore quia.
-      Explicabo sint dolor ullam eum ipsa nostrum ut ex neque cumque. Lorem,
-      ipsum dolor sit amet consectetur adipisicing elit. Nihil eaque vitae enim
-      quibusdam illo, aut unde iste, fugiat quasi sed doloremque tempore atque
-      laboriosam voluptatum inventore hic maiores perspiciatis voluptate.
+      <Featured type={`series`} />
     </div>
   );
 };
